5f4fd5c5a1e2f1c45553ecd741f4d772e001f1ed
Aqil Rizwan <aqil.rizwan@hotmail.co.uk>
2018-11-07 18:25:17 +0000

    Created a file to allow random networks to be generated. The central file was also modified to represent this.

--- Complex_Networks.py
+++ Complex_Networks.py
@@ -3,8 +3,10 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 import Regular_Network
-#import Random_Network
+import Random_Network
 #import SmallWorld_Network
+
+#import Network_Analysis
 
 
 # Define the number of nodes.
@@ -15,14 +17,22 @@ numberOfNodes = 20
 # Define the adjacency matrix.
 
 adjacencyMatrix_RegularNetwork = Regular_Network.GenerateAdjacencyMatrix(numberOfNodes)
+adjacencyMatrix_RandomNetwork = Random_Network.GenerateAdjacencyMatrix(numberOfNodes, 0.5)
 
 
 # Create a networkx graph.
 
-G_RegularNetwork = nx.from_numpy_matrix(adjacencyMatrix)
+G_RegularNetwork = nx.from_numpy_matrix(adjacencyMatrix_RegularNetwork)
+G_RandomNetwork = nx.from_numpy_matrix(adjacencyMatrix_RandomNetwork)
 
 
-# Plot the networkx graph.
+# Plot the regular networkx graph.
 
-plt.figure()
+plt.figure("Regular Network")
 nx.draw(G_RegularNetwork, with_labels = True, font_weight = 'bold')
+
+
+# PLot the random networkx graph.
+
+plt.figure("Random Network")
+nx.draw(G_RandomNetwork, with_labels = True, font_weight = 'bold')
---
+++ Random_Network.py
@@ -0,0 +1,34 @@
+import numpy as np
+import random
+
+
+# Create an adjacency matrix which corresponds to a random network.
+
+def GenerateAdjacencyMatrix(numberOfNodes, probability):
+    
+    # Initialise an empty maxtrix.
+    
+    adjacencyMatrix = np.zeros((numberOfNodes, numberOfNodes))
+    
+    
+    # Go through each lower diagonal element of the square matrix (do not want
+    # to go through the same link twice).
+    
+    for i in range (0, numberOfNodes):
+        for j in range (0, numberOfNodes):
+            
+            # Generate a random value between 0 to 1.
+            
+            randomValue = random.uniform(0, 1)
+            
+            # If in the lower diagonal, compare the probability and the given
+            # threshold to determine if a link between the two nodes should be
+            # made or not.
+            
+            if i >= j:
+                if randomValue >= probability:
+                    adjacencyMatrix[i, j] = 1
+                    adjacencyMatrix[j, i] = 1
+
+    return adjacencyMatrix
+
