62c68827e30d361f33c243d1ee0365daf4463ee0
Aqil Rizwan <aqil.rizwan@hotmail.co.uk>
2018-12-04 19:45:51 +0000

    File has been added to contain the small-world network functionality. Note that no specific functions have been added at this point.

---
+++ SmallWorld_Network.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+
+
+# Create an adjacency matrix which corresponds to a small world network.
+
+def GenerateAdjacencyMatrix(numberOfNodes):
+    
+    # Initialise an empty maxtrix.
+    
+    adjacencyMatrix = np.zeros((numberOfNodes, numberOfNodes))
+    
+    
+    
+
+
